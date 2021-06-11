e702defa954856d2bc9d75c2be01d9d1c37a6a6e
YaroslavSolo <yaroslav.solo@yandex.ru>
2021-06-11 21:04:32 +0300

    style fix #2

--- module-1/homework/Variant/variant.h
+++ module-1/homework/Variant/variant.h
@@ -9,13 +9,13 @@ template <typename... Types>
 class Variant;
 
 // -------Union-------
-template<size_t Idx, typename... Tail>
+template <size_t Idx, typename... Tail>
 union StoreUnion;
 
-template<size_t Idx>
+template <size_t Idx>
 union StoreUnion<Idx> {};
 
-template<size_t Idx, typename Head, typename... Tail>
+template <size_t Idx, typename Head, typename... Tail>
 union StoreUnion<Idx, Head, Tail...> {
 public:
     Head head;
@@ -24,7 +24,7 @@ public:
 
     StoreUnion() : tail() {
         if (!std::is_trivially_constructible_v<Head>) {
-            new (&head)Head();
+            new (&head) Head();
         }
     }
 
@@ -36,14 +36,13 @@ public:
 };
 // -------Union-------
 
-
 struct UnionHelper {
-    template<typename U>
+    template <typename U>
     static constexpr auto&& Get(U&& v, std::in_place_index_t<0>) {
         return std::forward<U>(v).head;
     }
 
-    template<typename U, size_t Idx>
+    template <typename U, size_t Idx>
     static constexpr auto&& Get(U&& v, std::in_place_index_t<Idx>) {
         return Get(std::forward<U>(v).tail, std::in_place_index<Idx - 1>);
     }
@@ -59,20 +58,19 @@ struct UnionHelper {
     }
 };
 
-
 // -------TypeList-------
-template<typename...>
+template <typename...>
 struct TypeList {};
 
-template<size_t Idx, typename TList>
+template <size_t Idx, typename TList>
 struct TypeAt {};
 
-template<typename Head, typename... Tail>
+template <typename Head, typename... Tail>
 struct TypeAt<0, TypeList<Head, Tail...>> {
     typedef Head target_type;
 };
 
-template<size_t Idx, typename Head, typename... Tail>
+template <size_t Idx, typename Head, typename... Tail>
 struct TypeAt<Idx, TypeList<Head, Tail...>> {
     typedef typename TypeAt<Idx - 1, TypeList<Tail...>>::target_type target_type;
 };
@@ -89,7 +87,6 @@ struct VariantAlternative<Idx, Variant<Types...>> {
 };
 // -------TypeList-------
 
-
 template <typename... Types>
 class Variant {
 public:
@@ -99,7 +96,7 @@ public:
     template <typename T>
     Variant& operator=(T&& t) noexcept;
 
-    template<typename T, typename... OtherTypes>
+    template <typename T, typename... OtherTypes>
     friend auto&& GenericGet(Variant<OtherTypes...>& v);
 
 private:
@@ -107,10 +104,10 @@ private:
 };
 
 // -------FindExactlyOneT-------
-const static size_t kNotFound = - 1;
+const static size_t kNotFound = -1;
 const static size_t kAmbiguity = kNotFound - 1;
 
-template<size_t SizeofFound>
+template <size_t SizeofFound>
 constexpr size_t CheckDuplicatesHelper(size_t cur_pos, size_t res, const bool (&exact)[SizeofFound],
                                        const bool (&convertible)[SizeofFound]) {
     if (res == kAmbiguity) {
@@ -132,38 +129,40 @@ constexpr size_t CheckDuplicatesHelper(size_t cur_pos, size_t res, const bool (&
     return res;
 }
 
-template<size_t SizeofFound>
+template <size_t SizeofFound>
 constexpr size_t CheckDuplicates(size_t cur_pos, const bool (&exact)[SizeofFound],
                                  const bool (&convertible)[SizeofFound]) {
     if (cur_pos == SizeofFound) {
         return kNotFound;
     }
 
-    return CheckDuplicatesHelper(cur_pos, CheckDuplicates(cur_pos + 1, exact, convertible), exact, convertible);
+    return CheckDuplicatesHelper(cur_pos, CheckDuplicates(cur_pos + 1, exact, convertible), exact,
+            convertible);
 }
 
-template<typename TargetType, typename... Types>
+template <typename TargetType, typename... Types>
 struct FindExactlyOneChecked {
-    constexpr static bool kFoundExact[sizeof...(Types)] = {std::is_same<TargetType, Types>::value...};
-    constexpr static bool kFoundConvertible[sizeof...(Types)] = {std::is_convertible<TargetType, Types>::value...};
-    constexpr static size_t kValue = CheckDuplicates(0, kFoundExact, kFoundConvertible);
+    constexpr static bool kExact[sizeof...(Types)] = {std::is_same<TargetType, Types>::value...};
+    constexpr static bool kConvertible[sizeof...(Types)] = {
+            std::is_convertible<TargetType, Types>::value...};
+    constexpr static size_t kValue = CheckDuplicates(0, kExact, kConvertible);
 
     static_assert(kValue != kNotFound, "no such type in parameter list");
-    static_assert(kValue != kAmbiguity, "there are several occurrences of the same type in parameter list");
+    static_assert(kValue != kAmbiguity,
+            "there are several occurrences of the same type in parameter list");
 };
 
-template<typename T>
+template <typename T>
 struct FindExactlyOneChecked<T> {
     static_assert(!std::is_same<T, T>::value, "type not in empty type list");
 };
 
-template<typename TargetType, typename... Types>
+template <typename TargetType, typename... Types>
 struct FindExactlyOneT : public FindExactlyOneChecked<TargetType, Types...> {};
 // -------FindExactlyOneT-------
 
-
-template<typename... Types>
-template<typename T>
+template <typename... Types>
+template <typename T>
 Variant<Types...>& Variant<Types...>::operator=(T&& t) noexcept {
     constexpr size_t kIdx = FindExactlyOneT<T, Types...>::kValue;
     UnionHelper::Set<kIdx>(std::forward<T>(t), std::in_place_index<kIdx>, data_);
@@ -172,7 +171,7 @@ Variant<Types...>& Variant<Types...>::operator=(T&& t) noexcept {
 }
 
 // Non-member functions
-template<typename T, typename... Types>
+template <typename T, typename... Types>
 auto&& GenericGet(Variant<Types...>& v) {
     return UnionHelper::Get(std::forward<Variant<Types...>>(v).data_,
                             std::in_place_index<FindExactlyOneT<T, Types...>::kValue>);
@@ -198,4 +197,4 @@ constexpr T&& Get(Variant<Types...>&& v) {
     return GenericGet<T>(std::move(v));
 }
 
-} // namespace task
+}  // namespace task
