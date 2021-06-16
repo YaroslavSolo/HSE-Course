97f71a597fa75da032a88e52dd2807536643eed7
YaroslavSolo <yaroslav.solo@yandex.ru>
2021-06-16 14:23:31 +0300

    fighting with clang

--- module-1/homework/Allocator/src/list/list.h
+++ module-1/homework/Allocator/src/list/list.h
@@ -31,9 +31,8 @@ public:
     // Special member functions
     List();
 
-    List(const List &other)
-        : allocator_(
-        node_allocator_traits::select_on_container_copy_construction(other.allocator_)) {
+    List(const List &other) {
+        allocator_ = node_allocator_traits::select_on_container_copy_construction(other.allocator_);
         root_ = allocator_.allocate(1);
         allocator_.construct(root_);
         root_->next = root_;
