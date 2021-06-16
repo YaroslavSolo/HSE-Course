72027b7b5d8fe22ef04bed1eded22e9cfa512cfa
YaroslavSolo <yaroslav.solo@yandex.ru>
2021-06-16 13:51:00 +0300

    fixed bugs

--- module-1/homework/Allocator/src/allocator/allocator.h
+++ module-1/homework/Allocator/src/allocator/allocator.h
@@ -18,7 +18,7 @@ public:
     using difference_type = ptrdiff_t;
     using propagate_on_container_copy_assignment = std::false_type;
     using propagate_on_container_move_assignment = std::false_type;
-    using propagate_on_container_swap = std::false_type;
+    using propagate_on_container_swap = std::true_type;
     using is_always_equal = std::false_type;
 
     CustomAllocator();
--- module-1/homework/Allocator/src/list/list.h
+++ module-1/homework/Allocator/src/list/list.h
@@ -4,7 +4,7 @@
 #include <memory>
 #include <type_traits>
 
-namespace task {
+//namespace task {
 
 template <typename T, typename Allocator = std::allocator<T>>
 class List {
@@ -15,8 +15,8 @@ public:
     class Iterator;
 
     using value_type = T;
-    using reference = T&;
-    using const_reference = const T&;
+    using reference = T &;
+    using const_reference = const T &;
     using size_type = size_t;
     using allocator_type = Allocator;
     using difference_type = ptrdiff_t;
@@ -31,11 +31,13 @@ public:
     // Special member functions
     List();
 
-    List(const List& other)
+    List(const List &other)
         : allocator_(node_allocator_traits::select_on_container_copy_construction(other.allocator_)) {
         root_ = allocator_.allocate(1);
         allocator_.construct(root_);
-        Node* cur = other.root_->next;
+        root_->next = root_;
+        root_->prev = root_;
+        Node *cur = other.root_->next;
 
         while (cur != other.root_) {
             PushBack(cur->value);
@@ -43,16 +45,16 @@ public:
         }
     }
 
-    List(const List& other, const Allocator& alloc);
+    List(const List &other, const Allocator &alloc);
 
-    List(List&& other);
-    List(List&& other, const Allocator& alloc);
+    List(List &&other) noexcept;
+    List(List &&other, const Allocator &alloc) noexcept;
 
     ~List();
 
-    List& operator=(const List& other);
+    List &operator=(const List &other);
 
-    List& operator=(List&& other) noexcept;
+    List &operator=(List &&other) noexcept;
 
     // Element access
     reference Front();
@@ -75,24 +77,24 @@ public:
 
     // Modifiers
     void Clear();
-    void Swap(List& other);
+    void Swap(List &other);
 
-    void PushBack(const T& value);
-    void PushBack(T&& value);
+    void PushBack(const T &value);
+    void PushBack(T &&value);
 
     template <typename... Args>
-    void EmplaceBack(Args&&... args);
+    void EmplaceBack(Args &&... args);
     void PopBack();
-    void PushFront(const T& value);
-    void PushFront(T&& value);
+    void PushFront(const T &value);
+    void PushFront(T &&value);
     template <typename... Args>
-    void EmplaceFront(Args&&... args);
+    void EmplaceFront(Args &&... args);
     void PopFront();
 
     void Resize(size_type count);
 
     // Operations
-    void Remove(const T& value);
+    void Remove(const T &value);
     void Unique();
     void Sort();
 
@@ -101,16 +103,16 @@ public:
     class Iterator {
     public:
         using value_type = T;
-        using reference = T&;
-        using const_reference = const T&;
-        using pointer = T*;
+        using reference = T &;
+        using const_reference = const T &;
+        using pointer = T *;
         using difference_type = ptrdiff_t;
         using iterator_category = std::bidirectional_iterator_tag;
 
-        explicit Iterator(Node* node) : node_(node) {
+        explicit Iterator(Node *node) : node_(node) {
         }
 
-        Iterator& operator++() {
+        Iterator &operator++() {
             node_ = node_->next;
             return *this;
         }
@@ -121,7 +123,7 @@ public:
             return temp;
         }
 
-        Iterator& operator--() {
+        Iterator &operator--() {
             node_ = node_->prev;
             return *this;
         }
@@ -132,11 +134,11 @@ public:
             return temp;
         }
 
-        bool operator==(const Iterator& rhs) {
+        bool operator==(const Iterator &rhs) {
             return node_ == rhs.node_;
         }
 
-        bool operator!=(const Iterator& rhs) {
+        bool operator!=(const Iterator &rhs) {
             return node_ != rhs.node_;
         }
 
@@ -149,7 +151,7 @@ public:
         }
 
     private:
-        Node* node_;
+        Node *node_;
     };
 
 private:
@@ -157,32 +159,32 @@ private:
     public:
         Node() = default;
 
-        explicit Node(value_type value, Node* prev = nullptr, Node* next = nullptr) noexcept {
+        explicit Node(value_type value, Node *prev = nullptr, Node *next = nullptr) noexcept {
             this->value = value;
             this->prev = prev;
             this->next = next;
         }
 
         template <typename... Args>
-        Node(Args&&... args, Node* prev, Node* next) {
+        Node(Args &&... args, Node *prev, Node *next) {
             this->value = value_type(std::forward<Args>(args)...);
             this->prev = prev;
             this->next = next;
         }
 
         value_type value;
-        Node* prev = nullptr;
-        Node* next = nullptr;
+        Node *prev = nullptr;
+        Node *next = nullptr;
     };
 
-    static std::pair<List<T, Allocator>*, List<T, Allocator>*> Split(const List<T, Allocator>& l);
+    static std::pair<List<T, Allocator> *, List<T, Allocator> *> Split(const List<T, Allocator> &l);
 
-    static List<T, Allocator> Merge(const List<T, Allocator>& left,
-                                    const List<T, Allocator>& right);
+    static List<T, Allocator> Merge(const List<T, Allocator> &left,
+                                    const List<T, Allocator> &right);
 
-    static List<T, Allocator> MergeSort(const List<T, Allocator>& l);
+    static List<T, Allocator> MergeSort(const List<T, Allocator> &l);
 
-    Node* root_ = nullptr;
+    Node *root_ = nullptr;
 
     size_type size_ = 0;
 
@@ -192,8 +194,10 @@ private:
 template <typename T, typename Allocator>
 List<T, Allocator>::~List() {
     Clear();
-    allocator_.destroy(root_);
-    allocator_.deallocate(root_, 1);
+    if (root_ != nullptr) {
+        allocator_.destroy(root_);
+        allocator_.deallocate(root_, 1);
+    }
 }
 
 template <typename T, typename Allocator>
@@ -205,7 +209,7 @@ List<T, Allocator>::List() {
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator>& List<T, Allocator>::operator=(const List& other) {
+List<T, Allocator> &List<T, Allocator>::operator=(const List &other) {
     if (this == &other) {
         return *this;
     }
@@ -218,7 +222,7 @@ List<T, Allocator>& List<T, Allocator>::operator=(const List& other) {
     }
 
     Clear();
-    Node* cur = other.root_->next;
+    Node *cur = other.root_->next;
     while (cur != other.root_) {
         PushBack(cur->value);
         cur = cur->next;
@@ -261,7 +265,7 @@ typename List<T, Allocator>::const_reference List<T, Allocator>::Back() const {
 
 template <typename T, typename Allocator>
 bool List<T, Allocator>::Empty() const noexcept {
-    return size_ == 0;
+    return size_ == 0 || root_ == nullptr;
 }
 
 template <typename T, typename Allocator>
@@ -276,7 +280,7 @@ void List<T, Allocator>::Clear() {
     }
 
     size_ = 0;
-    Node* cur = root_->next;
+    Node *cur = root_->next;
     while (cur != root_) {
         cur = cur->next;
         allocator_.destroy(cur->prev);
@@ -288,7 +292,7 @@ void List<T, Allocator>::Clear() {
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::PushBack(const T& value) {
+void List<T, Allocator>::PushBack(const T &value) {
     root_->prev->next = allocator_.allocate(1);
     allocator_.construct(root_->prev->next, value, root_->prev, root_);
     root_->prev = root_->prev->next;
@@ -301,7 +305,7 @@ void List<T, Allocator>::PopBack() {
         return;
     }
 
-    Node* target = root_->prev;
+    Node *target = root_->prev;
     target->prev->next = root_;
     root_->prev = target->prev;
     allocator_.destroy(target);
@@ -310,7 +314,7 @@ void List<T, Allocator>::PopBack() {
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::PushFront(const T& value) {
+void List<T, Allocator>::PushFront(const T &value) {
     root_->next->prev = allocator_.allocate(1);
     allocator_.construct(root_->next->prev, value, root_, root_->next);
     root_->next = root_->next->prev;
@@ -323,7 +327,7 @@ void List<T, Allocator>::PopFront() {
         return;
     }
 
-    Node* target = root_->next;
+    Node *target = root_->next;
     target->next->prev = root_;
     root_->next = target->next;
     allocator_.destroy(target);
@@ -343,31 +347,36 @@ void List<T, Allocator>::Resize(size_t count) {
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::Swap(List<T, Allocator>& other) {
-    std::swap(root_, other.root_);
-    std::swap(size_, other.size_);
+void List<T, Allocator>::Swap(List<T, Allocator> &other) {
     bool swap_alloc = node_allocator_traits::propagate_on_container_swap::value;
 
-    if (!swap_alloc && allocator_ != other.allocator_) {
+    if (!swap_alloc && allocator_ != other.allocator_ &&
+        !node_allocator_traits::is_always_equal::value) {
         throw std::runtime_error("Swapping lists with different allocators without propagation");
     }
 
     if (swap_alloc) {
+        std::swap(root_, other.root_);
+        std::swap(size_, other.size_);
         std::swap(allocator_, other.allocator_);
+    } else {
+        List<T, Allocator> temp = std::move(other);
+        other = std::move(*this);
+        *this = std::move(temp);
     }
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::Remove(const T& value) {
+void List<T, Allocator>::Remove(const T &value) {
     if (Empty()) {
         return;
     }
 
-    Node* cur = root_->next;
+    Node *cur = root_->next;
     while (cur != root_) {
         cur = cur->next;
         if (cur->prev->value == value) {
-            Node* target = cur->prev;
+            Node *target = cur->prev;
             target->prev->next = cur;
             target->next->prev = target->prev;
             allocator_.destroy(target);
@@ -383,12 +392,12 @@ void List<T, Allocator>::Unique() {
         return;
     }
 
-    Node* cur = root_->next->next;
+    Node *cur = root_->next->next;
     value_type cur_value = root_->next->value;
 
     while (cur != root_) {
         if (cur->value == cur_value) {
-            Node* target = cur;
+            Node *target = cur;
             target->prev->next = cur->next;
             target->next->prev = target->prev;
             allocator_.destroy(target);
@@ -407,11 +416,11 @@ void List<T, Allocator>::Sort() {
 }
 
 template <typename T, typename Allocator>
-std::pair<List<T, Allocator>*, List<T, Allocator>*> List<T, Allocator>::Split(const List& l) {
-    List<T, Allocator>* left = new List<T, Allocator>();
-    List<T, Allocator>* right = new List<T, Allocator>();
+std::pair<List<T, Allocator> *, List<T, Allocator> *> List<T, Allocator>::Split(const List &l) {
+    List<T, Allocator> *left = new List<T, Allocator>();
+    List<T, Allocator> *right = new List<T, Allocator>();
 
-    Node* cur = l.root_->next;
+    Node *cur = l.root_->next;
     for (size_t i = 0; i < l.Size() / 2; ++i, cur = cur->next) {
         left->PushBack(std::move(cur->value));
     }
@@ -424,11 +433,11 @@ std::pair<List<T, Allocator>*, List<T, Allocator>*> List<T, Allocator>::Split(co
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator> List<T, Allocator>::Merge(const List<T, Allocator>& left,
-                                             const List<T, Allocator>& right) {
+List<T, Allocator> List<T, Allocator>::Merge(const List<T, Allocator> &left,
+                                             const List<T, Allocator> &right) {
     List<T, Allocator> res;
-    Node* cur_left = left.root_->next;
-    Node* cur_right = right.root_->next;
+    Node *cur_left = left.root_->next;
+    Node *cur_right = right.root_->next;
 
     while (cur_left != left.root_ && cur_right != right.root_) {
         if (cur_right->value < cur_left->value) {
@@ -454,7 +463,7 @@ List<T, Allocator> List<T, Allocator>::Merge(const List<T, Allocator>& left,
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator> List<T, Allocator>::MergeSort(const List<T, Allocator>& l) {
+List<T, Allocator> List<T, Allocator>::MergeSort(const List<T, Allocator> &l) {
     if (l.Size() > 1) {
         auto halves = Split(l);
         return Merge(MergeSort(*halves.first), MergeSort(*halves.second));
@@ -464,10 +473,10 @@ List<T, Allocator> List<T, Allocator>::MergeSort(const List<T, Allocator>& l) {
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator>::List(const List& other, const Allocator& alloc)
+List<T, Allocator>::List(const List &other, const Allocator &alloc)
     : allocator_(alloc), root_(allocator_.allocate(1)) {
     allocator_.construct(root_);
-    Node* cur = other.root_->next;
+    Node *cur = other.root_->next;
 
     while (cur != other.root_) {
         PushBack(cur->value);
@@ -476,20 +485,36 @@ List<T, Allocator>::List(const List& other, const Allocator& alloc)
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator>::List(List<T, Allocator>&& other) : allocator_(std::move(other.allocator_)) {
+List<T, Allocator>::List(List<T, Allocator> &&other) noexcept {
+    Clear();
+    if (node_allocator_traits::is_always_equal::value || allocator_ == other.allocator_) {
         root_ = other.root_;
         size_ = other.size_;
+    } else if (node_allocator_traits::propagate_on_container_move_assignment::value) {
+        allocator_ = std::move(other.allocator_);
+        root_ = other.root_;
+        size_ = other.size_;
+    } else {
+        for (auto iter = other.Begin(); iter != other.End(); ++iter) {
+            PushBack(std::move(*iter));
+        }
+    }
+
     other.root_ = nullptr;
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator>::List(List<T, Allocator>&& other, const Allocator& alloc)
-        : allocator_(alloc), root_(other.root_), size_(other.size_) {
+List<T, Allocator>::List(List<T, Allocator> &&other, const Allocator &alloc) noexcept {
+    Clear();
+    allocator_ = alloc;
+    for (auto iter = other.Begin(); iter != other.End(); ++iter) {
+        PushBack(std::move(*iter));
+    }
     other.root_ = nullptr;
 }
 
 template <typename T, typename Allocator>
-List<T, Allocator>& List<T, Allocator>::operator=(List<T, Allocator>&& other) noexcept {
+List<T, Allocator> &List<T, Allocator>::operator=(List<T, Allocator> &&other) noexcept {
     Clear();
     if (node_allocator_traits::is_always_equal::value || allocator_ == other.allocator_) {
         root_ = other.root_;
@@ -516,12 +541,12 @@ typename List<T, Allocator>::size_type List<T, Allocator>::MaxSize() const noexc
 
 template <typename T, typename Allocator>
 typename List<T, Allocator>::iterator List<T, Allocator>::Begin() noexcept {
-    return task::List<T, Allocator>::iterator(root_->next);
+    return task::List<T, Allocator>::iterator(root_ == nullptr ? nullptr : root_->next);
 }
 
 template <typename T, typename Allocator>
 typename List<T, Allocator>::const_iterator List<T, Allocator>::Begin() const noexcept {
-    return task::List<T, Allocator>::const_iterator(root_->next);
+    return task::List<T, Allocator>::const_iterator(root_ == nullptr ? nullptr : root_->next);
 }
 
 template <typename T, typename Allocator>
@@ -535,7 +560,7 @@ typename List<T, Allocator>::const_iterator List<T, Allocator>::End() const noex
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::PushBack(T&& value) {
+void List<T, Allocator>::PushBack(T &&value) {
     root_->prev->next = allocator_.allocate(1);
     allocator_.construct(root_->prev->next, std::forward<T>(value), root_->prev, root_);
     root_->prev = root_->prev->next;
@@ -544,7 +569,7 @@ void List<T, Allocator>::PushBack(T&& value) {
 
 template <typename T, typename Allocator>
 template <typename... Args>
-void List<T, Allocator>::EmplaceBack(Args&&... args) {
+void List<T, Allocator>::EmplaceBack(Args &&... args) {
     root_->prev->next = allocator_.allocate(1);
     allocator_.construct(root_->prev->next, std::forward<Args>(args)..., root_->prev, root_);
     root_->prev = root_->prev->next;
@@ -552,7 +577,7 @@ void List<T, Allocator>::EmplaceBack(Args&&... args) {
 }
 
 template <typename T, typename Allocator>
-void List<T, Allocator>::PushFront(T&& value) {
+void List<T, Allocator>::PushFront(T &&value) {
     root_->next->prev = allocator_.allocate(1);
     allocator_.construct(root_->next->prev, std::forward<T>(value), root_, root_->next);
     root_->next = root_->next->prev;
@@ -561,7 +586,7 @@ void List<T, Allocator>::PushFront(T&& value) {
 
 template <typename T, typename Allocator>
 template <typename... Args>
-void List<T, Allocator>::EmplaceFront(Args&&... args) {
+void List<T, Allocator>::EmplaceFront(Args &&... args) {
     root_->next->prev = allocator_.allocate(1);
     allocator_.construct(root_->next->prev, std::forward<Args>(args)..., root_, root_->next);
     root_->next = root_->next->prev;
